01dc314e93dfca18a232dd587cba83962cb42813
Finnal project <259657@student.pwr.edu.pl>
2024-03-16 20:13:04 +0100

    łączenie z baza danych + dodawanie informacji przez aplikacje

--- app/src/main/java/com/example/temiv3/MainActivity.java
+++ app/src/main/java/com/example/temiv3/MainActivity.java
@@ -1,17 +1,44 @@
 package com.example.temiv3;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.robotemi.sdk.Robot;
 import com.robotemi.sdk.listeners.OnRobotReadyListener;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class MainActivity extends AppCompatActivity implements OnRobotReadyListener {
+
+
+
+    @SuppressLint("NewApi")
+    public Connection connectionclass() {
+        Connection con = null;
+        String ip = "172.16.0.115", port = "1433", username = "sa", password = "Oliwia123!@", databasename = "Temi";
+        StrictMode.ThreadPolicy tp = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(tp);
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            String connectionUrl = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";databasename=" + databasename + ";User=" + username + ";password=" + password + ";";
+            con = DriverManager.getConnection(connectionUrl);
+        } catch (Exception exception) {
+            Log.e("Error", exception.getMessage());
+        }
+        return con;
+    }
+
     public static final String TAG = MainActivity.class.getSimpleName();
     public static Robot mRobot;
 
@@ -29,6 +56,31 @@ public class MainActivity extends AppCompatActivity implements OnRobotReadyListe
             @Override
             public void onClick(View view) {
                 mRobot.showAppList();
+            }
+        });
+
+        TextView id = (TextView) findViewById(R.id.edittexid);
+        TextView name = (TextView) findViewById(R.id.edittextname);
+        TextView location = (TextView) findViewById(R.id.edittextlocation);
+        TextView description = (TextView) findViewById(R.id.edittextdescription);
+        Button btninsert = (Button) findViewById(R.id.btnadd);
+//        Button btnupdate = (Button) findViewById(R.id.btnupdate);
+//        Button btndelete = (Button) findViewById(R.id.btndelete);
+//        Button btnget = (Button) findViewById(R.id.btnget);
+
+        btninsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Connection connection = connectionclass();
+                try {
+                    if (connection != null) {
+                        String sqlinsert = "Insert into Garage values ('" + id.getText().toString() + "','" + name.getText().toString() + "','" + location.getText().toString() + "','" + description.getText().toString() + "')";
+                        Statement st = connection.createStatement();
+                        ResultSet rs = st.executeQuery(sqlinsert);
+                    }
+                } catch (Exception exception) {
+                    Log.e("Error", exception.getMessage());
+                }
             }
         });
     }
